8c5e3714eb480c0b45f6402f268970a0e7f7375d
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-12-19 14:07:38 +0100

    Added ggplot for iteration analysis

--- run/plot.R
+++ run/plot.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 daresult <- readRDS("../run/daresults/daresults.scenario-3.2700-580.3.2.1.2.rds")
 m <-1
 quota <- '80%'
@@ -28,5 +29,29 @@ segments(xend,y*complete*.975,xend+.1,y*complete*.975)
 text(bp[11]+1.2,2700*complete*0.975, '5%')
 
 dev.off()
-        
-  
+
+iteration <- as.vector(unlist(lapply(1:11,function(x){rep(x,3)})))
+type <- as.vector(rep(rownames(iterationtable),11))
+values <- as.vector(iterationtable*y)
+data <- data.frame(iteration,type,values)
+
+ggplot(data=data, aes(fill=factor(type, levels=c("new","altered","unchanged")), x=iteration, y=values)) +
+  geom_bar(position="stack", stat="identity", width=.8)  +
+  scale_fill_grey(start = 0.45, end = 0.8) +
+  xlab("Number of iterations") +
+  ylab("Places") +
+  ggtitle("Matching after X iterations in comparison to a full DA run") +
+  labs(subtitle=paste("(share of private facilities =",quota,")")) +
+  guides(fill=guide_legend(title="Legend")) +
+  theme_classic() + 
+  scale_x_continuous(breaks=1:11) + 
+  scale_y_continuous(breaks=c(0,450,900,1350,1800,2250,2700)) + 
+  geom_abline(slope=0, intercept=y*complete*.95,  col = "black",lty=2) +
+  theme(
+    legend.position = c(.925, .1),
+    legend.justification = c("right", "bottom"),
+    legend.box.just = "right",
+    legend.margin = margin(6, 6, 6, 6)
+  ) +
+ggsave("../run/plots/analysis2.png", scale=.85)
+
