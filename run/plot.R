2f85a35475ac8acd86597db28ab8b0ae267aa9ba
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2020-04-12 18:20:15 +0200

    New creation of analysis2

--- run/plot.R
+++ run/plot.R
@@ -1,8 +1,9 @@
 library(ggplot2)
-daresult <- readRDS("../run/daresults/daresults.scenario-3.2700-580.3.2.1.2.rds")
+#daresult <- readRDS("../run/daresults/daresults.scenario-3.2700-580.3.2.1.2.rds")
+daresult <- readRDS("../run/daresults/daresults.scenario-6.2000-600.rds")
 m <-1
 quota <- '80%'
-y <- 2700
+y <- 2000
 
 iteration <- daresult$iterations[[m]]
 iterationtable <- t(as.matrix(iteration[,-1]))
@@ -17,8 +18,8 @@ png("../run/plots/analysis.png", 480, 360)
 colors=c("darkgreen","darkblue","brown")
 par(mfrow=c(1, 1), mar=c(4, 4, 3, 3),xpd=TRUE)
 bp <- barplot(iterationtable * y,main=paste("Matching after X iterations in comparison to full run \n(private quota=",quota,")"),
-            xlab="Number of iterations", col=colors,ylim=c(0,y),yaxt='n',ylab='Places')
-legend(x=bp[8]*1.03,y=y*.3, row.names(iterationtable), fill=colors, bg='white')
+              xlab="Number of iterations", col=colors,ylim=c(0,y),yaxt='n',ylab='Places')
+legend(x=bp[8]*1.03,y=y*.3, row.names(iterationtable), fill=colors, bg='white', reverse=T)
 axis(side = 1, at = bp, tick = FALSE, labels=seq(iterationtable[1,]))
 axis(2,at=c(0,450,900,1350,1800,2250,2700),labels=TRUE)
 xend = (bp[11]+.7)
@@ -37,21 +38,27 @@ data <- data.frame(iteration,type,values)
 
 ggplot(data=data, aes(fill=factor(type, levels=c("new","altered","unchanged")), x=iteration, y=values)) +
   geom_bar(position="stack", stat="identity", width=.8)  +
-  scale_fill_grey(start = 0.45, end = 0.8) +
+  scale_fill_grey(start = 0.45, end = 0.8, labels = c("unmatched","temporary","final")) +
   xlab("Number of iterations") +
   ylab("Places") +
-  ggtitle("Matching after X iterations in comparison to a full DA run") +
-  labs(subtitle=paste("(share of private facilities =",quota,")")) +
-  guides(fill=guide_legend(title="Legend")) +
+  #ggtitle("Matching after X iterations in comparison to a full DA run") +
+  #labs(subtitle=paste("(share of private facilities =",quota,")")) +
+  guides(fill=guide_legend(title="State of assignments")) +
   theme_classic() + 
   scale_x_continuous(breaks=1:11) + 
-  scale_y_continuous(breaks=c(0,450,900,1350,1800,2250,2700)) + 
-  geom_abline(slope=0, intercept=y*complete*.95,  col = "black",lty=2) +
+  scale_y_continuous(breaks=c(0,400,800,1200,1600,2000)) + 
+  #geom_abline(slope=0, intercept=y*complete*.95,  col = "black",lty=2) +
+  geom_segment(mapping=aes(x=-1, y=y*complete*.95, xend=11.7, yend=y*complete*.95), color="black",lty="22",size=.25) +
+  geom_segment(mapping=aes(x=11.7, y=y*complete, xend=11.7, yend=y*complete*.95), color="black",lty="22",size=.25) +
+  geom_segment(mapping=aes(x=-1, y=y*complete-2, xend=11.7, yend=y*complete-2), color="black",lty="22",size=.25) +
+  geom_segment(mapping=aes(x=11.7, y=y*complete*.975, xend=11.85, yend=y*complete*.975), color="black",lty="22",size=.25) +
+  annotate("text",x=12.15,y=y*complete*.975,label="5%", size=3, fontface="plain") +
+  coord_cartesian(xlim=c(0.7,11.8)) +
   theme(
-    legend.position = c(.925, .1),
+    legend.position = c(.875, .1),
     legend.justification = c("right", "bottom"),
     legend.box.just = "right",
     legend.margin = margin(6, 6, 6, 6)
   ) +
-ggsave("../run/plots/analysis2.png", scale=.85)
+ggsave("../run/plots/analysis2.pdf", scale=.85, width=7, height = 5)
 
